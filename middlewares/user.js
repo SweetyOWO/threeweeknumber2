5a7cb7a22beebe1c79ad7954ae4e625106f00e54
SweetyOWO <36s@mail.ru>
2024-05-01 19:25:43 +0300

    efwfw

--- middlewares/user.js
+++
@@ -1,9 +0,0 @@
-const users = require('../models/user');
-
-const findAllUsers = async (req, res, next) => {
-    console.log('GET /users')
-    req.usersArray = await users.find({});
-    next()
-}
-
-module.exports = findAllUsers
